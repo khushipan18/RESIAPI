fea0d59d6b98117bff7465425bc1593c5f5f051c
khushbu <khushipan18@gmail.com>
2020-03-23 15:38:24 -0400

    Created Model -a

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::apiResource('/Products', 'ProductController');
+
+Route::group(['prefix'  => 'products'],function (){
+
+     Route::apiResource('/{product}/reviews','ReviewController');
+
+
+});
