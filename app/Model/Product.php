63465c786d83e1bea48304d943134d7af66b897b
khushbu <khushipan18@gmail.com>
2020-03-24 15:28:50 -0400

    relationship created

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -1,10 +1,13 @@
 <?php
 
 namespace App\Model;
+use App\Model\Review;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    public  function revieew(){
+        return $this->hasMany(Review::class);
+    }
 }
--- app/Model/Review.php
+++ app/Model/Review.php
@@ -1,10 +1,18 @@
 <?php
 
 namespace App\Model;
+use App\Model\Product;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    
+  public function product(){
+
+  return $this->belongsTo(product :: class);
+
+  }
+
+
 }
